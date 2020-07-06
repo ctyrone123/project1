bc38fb2a604ec586c8d88cd3454948ede0634d02
Ctyrone123 <Ctyrone123@gmail.com>
2020-07-05 20:07:52 -0500

    api calls

---
+++ assets/js/recipe.js
@@ -0,0 +1,42 @@
+function getrecipe(q) {
+    $.ajax({
+        url: "https://api.spoonacular.com/recipes/search?apiKey=a568c4a88785422fbf4cf46b976c40e8&number=3&query=" + q,
+        success: function (res) {
+            for (var i = 0; i < 3; i++) {
+                document.getElementById("output").innerHTML += "<h1>" + res.results[i].title + "</h1><br><a href='" + res.results[i].sourceUrl + "'><img src='" + res.baseUri + res.results[i].image + "' width='400' /></a><br>Ready in " + res.results[i].readyInMinutes + " minutes"
+                console.log(q);
+            }
+        }
+    });
+}
+
+function renderCocktail(){
+
+      var queryURLCocktail = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+      $.ajax({
+      url:queryURLCocktail,
+      method: "GET"
+    }).then(function(response) {
+      console.log("response", response);
+      var drink = response.drinks[0];
+      var name = drink.strDrink;
+      var image = drink.strDrinkThumb;
+      var n = 0;
+      var ingredients = [];
+      var directions = drink.strInstructions;
+      while (n<15) {
+        if (drink[`strIngredient${n+1}`] === null){
+          break;
+        };
+        ingredients.push(drink[`strIngredient${n+1}`]+": " + drink[`strMeasure${n+1}`]);
+        ingredients[n] = ingredients[n].replace(": null", "");
+        n++;
+      };
+      $("#name").html(name);
+      $("#image").html(`<img src='${image}'/>`);
+      $("#ingredients").html(ingredients.map(ingredient => `<li class="cocktail">${ingredient}</li>`).join(""));
+      $("#directions").html(directions);
+      $("#idk").html(`<button id="random-cocktail" class="idk btn" type="submit" onclick="renderCocktail(document.getElementById('random-cocktail').value)">Try Again!</button>`);
+      console.log(ingredients);
+    });
+  }
